c4a012b93d233df5aa2ddff43758d3252f0159b4
shxx91 <56170685+shxx91@users.noreply.github.com>
2021-03-20 08:08:15 -0300

    Add files via upload

---
+++ geocoding.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import json
+import folium
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+from geopy import distance
+
+df = pd.read_csv('/Users/anaferreira/Downloads/Address_List_100.csv', encoding='UTF8')
+
+df['Street Number'] = df['Street Number'].astype(str)
+
+df['Full Address'] = df['Street Number'] + str(', ') + df['Street Name'] + str(', ') + df['Zipcode'] + str(', ') + df['City'] + str(', ') + df['State']+ str(', ') + df['Country']
+
+geocoder = RateLimiter(Nominatim(user_agent='user290').geocode, min_delay_seconds=1)
+df['Location'] = df['Full Address'].apply(geocoder)
+
+df['Latitude'] = df['Location'].apply(lambda loc: loc.latitude if loc else None)
+df['Longitude'] = df['Location'].apply(lambda loc: loc.longitude if loc else None)
+
+df.to_csv('/Users/anaferreira/Downloads/Address_List_100_coded.csv', encoding='UTF8')
+
+#dropar as nans pra plotar no mapa
+df = df.dropna(subset=['Longitude'])
+df = df.dropna(subset=['Latitude'])
+
+for i in range(0,len(df)):
+   folium.Marker(
+      location=[df.iloc[i]['Latitude'], df.iloc[i]['Longitude']],
+      popup=df.iloc[i]['Full Address'],
+   ).add_to(m)
+
+m
+
+
+
+
